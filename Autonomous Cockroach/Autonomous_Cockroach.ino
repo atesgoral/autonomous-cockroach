348b4242e62fe93a42155cd25f1dcba423356d1f
Ates Goral <ates.g@myplanetdigital.com>
2015-02-08 11:11:12 -0500

    Rudimentary initial implementation

--- Autonomous Cockroach/Autonomous_Cockroach.ino
+++ Autonomous Cockroach/Autonomous_Cockroach.ino
@@ -44,51 +44,167 @@
 #error Platform not defined
 #endif // end IDE
 
-// Include application, user and local libraries
+const uint8_t ledPin = 13;
 
+const uint8_t controlPin1 = 3;
+const uint8_t controlPin2 = 9;
+const uint8_t controlPin3 = 10;
+const uint8_t controlPin4 = 11;
 
-// Define variables and constants
-//
-// Brief	Name of the LED
-// Details	Each board has a LED but connected to a different pin
-//
-uint8_t myLED;
+const uint8_t lightSensorPin1 = A0;
+const uint8_t lightSensorPin2 = A1;
 
-
-//
-// Brief	Setup
-// Details	Define the pin the LED is connected to
-//
-// Add setup code
-void setup() {
-    // myLED pin number
-#if defined(ENERGIA) // All LaunchPads supported by Energia
-    myLED = RED_LED;
-#elif defined(DIGISPARK) // Digispark specific
-    myLED = 1; // assuming model A
-#elif defined(MAPLE_IDE) // Maple specific
-    myLED = BOARD_LED_PIN;
-#elif defined(WIRING) // Wiring specific
-    myLED = 15;
-#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
-    myLED = 10;
-#elif defined(SPARK) // Spark specific
-    myLED = D7;
-#else // Arduino, chipKIT, Teensy specific
-    myLED = 13;
-#endif
-    
-    pinMode(myLED, OUTPUT);
+void steerCenter() {
+    digitalWrite(controlPin1, LOW);
+    digitalWrite(controlPin2, LOW);
 }
 
-//
-// Brief	Loop
-// Details	Blink the LED
-//
-// Add loop code
+void steerRight(double amount) {
+    analogWrite(controlPin1, amount * 255.0 + 0.5);
+    digitalWrite(controlPin2, LOW);
+}
+
+void steerLeft(double amount) {
+    digitalWrite(controlPin1, LOW);
+    analogWrite(controlPin2, amount * 255.0 + 0.5);
+}
+
+void steer(double amount) {
+    if (amount >= 0) {
+        steerRight(amount);
+    } else {
+        steerLeft(-amount);
+    }
+}
+
+void stop() {
+    digitalWrite(controlPin3, LOW);
+    digitalWrite(controlPin4, LOW);
+}
+
+void forward(double amount) {
+    digitalWrite(controlPin3, LOW);
+    analogWrite(controlPin4, amount * 255.0 + 0.5);
+}
+
+void back(double amount) {
+    analogWrite(controlPin3, amount * 255.0 + 0.5);
+    digitalWrite(controlPin4, LOW);
+}
+
+void move(double amount) {
+    if (amount >= 0) {
+        forward(amount);
+    } else {
+        back(-amount);
+    }
+}
+
+void ledOn() {
+    digitalWrite(ledPin, HIGH);
+}
+
+void ledOff() {
+    digitalWrite(ledPin, LOW);
+}
+
+class LightSensor {
+    int pin;
+    float baselineSampleSum;
+    int baselineSampleCount;
+
+    int readRaw() {
+        int raw = analogRead(this->pin);
+        delay(5);
+        return raw;
+    }
+public:
+    LightSensor(int pin) {
+        this->pin = pin;
+        this->baselineSampleSum = 0.0;
+        this->baselineSampleCount = 0;
+    }
+
+    void sampleBaseline() {
+        this->baselineSampleSum += this->readRaw();
+        this->baselineSampleCount++;
+    }
+
+    float read() {
+        float baseline = this->baselineSampleSum / this->baselineSampleCount;
+        float raw = this->readRaw();
+
+        if (raw < baseline) {
+            raw = baseline;
+        }
+
+        return (raw - baseline) / 1023;
+    }
+};
+
+LightSensor rightLightSensor(lightSensorPin1);
+LightSensor leftLightSensor(lightSensorPin2);
+
+void calibrate() {
+    ledOn();
+
+    while (millis() < 5000) {
+        rightLightSensor.sampleBaseline();
+        leftLightSensor.sampleBaseline();
+    }
+
+    ledOff();
+}
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(ledPin, OUTPUT);
+
+    pinMode(controlPin1, OUTPUT);
+    pinMode(controlPin2, OUTPUT);
+    pinMode(controlPin3, OUTPUT);
+    pinMode(controlPin4, OUTPUT);
+
+    stop();
+    steerCenter();
+    ledOff();
+
+    calibrate();
+    //forward(1.0);
+}
+
 void loop() {
-    digitalWrite(myLED, HIGH);
-    delay(500);
-    digitalWrite(myLED, LOW);
-    delay(500);
+    float rightLight = rightLightSensor.read();
+    float leftLight = leftLightSensor.read();
+
+    float maxLight = rightLight > leftLight ? rightLight : leftLight;
+    float lightDelta = rightLight - leftLight;
+    float absLightDelta = lightDelta > 0 ? lightDelta : -lightDelta;
+
+    Serial.print("Max: ");
+    Serial.print(maxLight);
+    Serial.print(" Delta: ");
+    Serial.print(lightDelta);
+    Serial.print(" Abs Delta: ");
+    Serial.print(absLightDelta);
+    Serial.println();
+
+    if (maxLight < 0.05) {
+        // Happily in the dark. Stop where you are.
+        steerCenter();
+        stop();
+    } else if (absLightDelta < 0.05) {
+        // Not enough directional light difference to steer away, run backwards.
+        steerCenter();
+        back(1.0);
+    } else {
+        // Run towards dark.
+        if (lightDelta > 0) {
+            steerLeft(1.0);
+        } else {
+            steerRight(1.0);
+        }
+        forward(1.0);
+    }
 }
